906bf540cdb3883ded49a2eb89830865ddc88ea2
egordmitriev-creator <edmitriev377@gmail.com>
2025-04-23 15:49:17 +0700

    hw_9

---
+++ Homework_9/Service_auth/src/main/java/da4a/service_auth/model/UserRole.java
@@ -0,0 +1,6 @@
+package da4a.service_auth.model;
+
+public enum UserRole {
+    ROLE_CUSTOMER,
+    ROLE_ADMIN
+}
---
+++ Homework_9/Service_data/src/main/java/da4a/service_data/controller/OrderController.java
@@ -0,0 +1,22 @@
+package da4a.service_data.controller;
+
+import da4a.service_data.model.Order;
+import da4a.service_data.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        return ResponseEntity.ok(orderService.save(order));
+    }
+}
