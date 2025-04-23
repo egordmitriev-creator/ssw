5f9963b099e8997c8a896e7b24ecbba795cc8c2d
egordmitriev-creator <edmitriev377@gmail.com>
2025-04-23 16:43:09 +0700

    PetService

--- PetServiceWithBD/src/main/java/com/example/PetServiceWithBD/repository/PetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.PetServiceWithBD.repository;
-
-import com.example.PetServiceWithBD.model.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetRepository extends JpaRepository<Pet, Long> {
-}
--- PetServiceWithBD/src/main/java/com/example/PetServiceWithBD/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.PetServiceWithBD.repository;
-
-import com.example.PetServiceWithBD.model.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
